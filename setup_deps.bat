73bc7f7cb16396e20cd632229b2f73785bd1ab6b
Dmytro Lytovchenko <dmytro.lytovchenko@gmail.com>
2023-12-24 14:38:53 +0100

    Deps cloned into github org

---
+++ setup_deps.bat
@@ -0,0 +1,4 @@
+mkdir deps
+git clone https://github.com/ironclause/erl_parse.git --branch 0.0.8
+git clone https://github.com/ironclause/erl_pp.git --branch 0.1
+git clone https://github.com/ironclause/erl_tokenize.git --branch 0.3
